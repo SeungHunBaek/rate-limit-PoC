94a93dd60085253a6b4116c993365bd0518d7f2c
baek <xemeal@gmail.com>
2022-09-26 23:36:29 +0900

    부하테스트

--- src/app.js
+++ src/app.js
@@ -1,9 +1,38 @@
 const express = require('express'); 
 const app = express();
-const rateLimit = require('express-rate-limit')
+// const redisClient = redis.createClient(6379,"127.0.0.1");
+const redisClient = require('redis').createClient({
+  // host : '192.168.219.103',
+  // host : 'redis-server',
+  // host : 'host.docker.internal',
+  host : '127.0.0.1',
+  port : 6379,
+  legacyMode: true
+});
+const { RateLimiterRedis } = require('rate-limiter-flexible');
+
+redisClient.connect();
+redisClient.on("ready", ()=> {
+  console.log("Redis is Ready");
+});
+
+redisClient.on("error", (err) => {
+  console.error(err);
+});
 
 
-app.get('/', function (req, res) {
+const accessLimiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'access',
+  points: 5, // 5 attempts
+  duration: 1, // 1 초당 5회, 1초마다 5개의 point가 생성 
+});
+
+app.get('/', async function (req, res) {
+
+  await redisClient.set( "name", "Bobby" );
+  console.log(await redisClient.get( "name" ));
+  
 	res.send('Hello World Express'); 
 }); 
 
