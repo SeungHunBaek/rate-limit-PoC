0b65429442b7f5ed6ee451dfeae30c430bdb0dc1
baek <xemeal@gmail.com>
2022-10-03 15:57:31 +0900

    test결과

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 const express = require('express'); 
 const app = express();
 const port = 4000;
-// const redis = require("./redis/redisService");
-// // const redis = require("redis");
-// // const client = redis.createClient(6379);
 
 const rateLimiterRedisMiddleware = require('./rateLimit/rateLimiterRedis');
 app.use(rateLimiterRedisMiddleware);
@@ -11,10 +8,7 @@ app.use(rateLimiterRedisMiddleware);
 let cnt = 0;
 app.get('/', async function (req, res) {
   try {
-      cnt++;
-      // redis.set( `name_${cnt}`, `Bobby_${cnt}`);
-      // console.log(cnt)
-      res.status(200).set("Content-Type", "text/html").send({a:cnt})
+      res.status(200).set("Content-Type", "text/html").send({a:++cnt})
   } catch (err) {
     console.log("err", err);
   }
@@ -24,4 +18,7 @@ process.on('uncaughtException', (err) =>{
   console.error(`[uncaughtException]: ${err}`);
 })
 
-app.listen(port)
+app.listen(port, ()=>{
+  process.send(`ready`);
+  console.log(`express ready`)
+})
--- src/rateLimit/rateLimiterRedis.js
+++ src/rateLimit/rateLimiterRedis.js
@@ -17,38 +17,45 @@ redisClient.on("ready", ()=> {
 });
 
 redisClient.on("error", (err) => {
-  console.error(`Redis Error: JSON.stringify(err, null, 2)`);
+  console.error(`Redis Error: ${JSON.stringify(err, null, 2)}`);
 });
 
 const accessLimiter = new RateLimiterRedis({
     storeClient: redisClient,
-    keyPrefix: "access_",
-    points: 30, // 사용가능한 포인트 
+    keyPrefix: 'ccxt-rate-limit-',
+    points: 2, // 사용가능한 포인트 
     duration: 1, // 충전되는 시간
     // blockDuration: 
 });
 
 const sleep = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
-
+__cnt = 0;
 const rateLimiterMiddleware = async (req, res, next) => {
     const IP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
+    const startTime = new Date().getTime() / 1000;
+    let _startTime = performance.now()
+    console.log(__cnt++);
     accessLimiter.consume(`${IP}`, 1)
     .then((_res) => {
         // console.log(`IP: ${IP}, response: ${_res}`);
-        console.log(`IP:${IP}, success`)
+        console.log(`[success]IP: ${IP}`);
         next();
+        const endTime = new Date().getTime() / 1000;
+        const _endTime = performance.now()
+        console.log(`[processing_time] ${endTime - startTime}ms`);
+        console.log(`[processing_time] ${_endTime - _startTime}ms`);
     })
     .catch(async (rejRes) => {
         // Redis가 에러를 return했을때
+        // bobby: 메모리로 대응하도록 해? 바로 에러 나는거 확인하고 대응하는게 좋을듯
         if (rejRes instanceof Error) {
             // Some Redis error
             // Never happen if `insuranceLimiter` set up
             // Decide what to do with it in other case
-            console.log(`IP: ${IP}, redis-error: ${rejRes}`);
+            console.log(`[Error]: IP: ${IP}, redis-error: ${rejRes}`);
         } else {
           // rateLimiterMiddleware(req, res, next);
-            // console.log(`IP:${IP}, error: ${rejRes}`);
+            // console.error(`IP:${IP}, error: ${rejRes}`);
             // // Can't consume
             // // If there is no error, rateLimiterRedis promise rejected with number of ms before next request allowed
             // res.set('Retry-After', String(secs));
