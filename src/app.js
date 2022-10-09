cbe9c63b8e166624cf0c84ed1e4adb51b2a4274c
baek <xemeal@gmail.com>
2022-10-09 12:18:38 +0900

    init

--- src/app.js
+++ src/app.js
@@ -1,39 +1,32 @@
-const express = require('express'); 
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const timeout = require('connect-timeout');
-const port = 4000;
 
+const app = express();
+const port = 4000;
 const rateLimiterRedisMiddleware = require('./rateLimit/rateLimiterRedis');
 
 let cnt = 0;
-// const haltOnTimedout = (req, res, next) => {
-//   if (!req.timedout) {
-//     console.log(`[app.js] req.timedout: ${req.timedout}`);
-//     next();
-//   } else {
-//     console.log(`[app.js] req.timedout: ${req.timedout}`);
-//     res.status(504).send(`[app.js] Gateway Timeout`); 
-//   }
-// }
 app.use(timeout('10s'));
-app.use(bodyParser.json({ extended: true }))
-// app.use(haltOnTimedout);
+app.use(bodyParser.json({ extended: true }));
 app.use(rateLimiterRedisMiddleware);
+
 app.get('/', async function (req, res) {
   try {
-      console.log(`[app.js] End`);
-      res.status(200).set("Content-Type", "text/html").send({a:++cnt})
+    console.log(`[app.js] End`);
+    res
+      .status(200)
+      .set('Content-Type', 'text/html')
+      .send({ a: (cnt += 1) });
   } catch (err) {
-    console.log("err", err);
+    console.log(`[/:Error], ${JSON.stringify(err, null, 2)}`);
   }
-}); 
+});
 
-process.on('uncaughtException', (err) =>{
+process.on('uncaughtException', (err) => {
   console.error(`[uncaughtException]: ${err}`);
-})
+});
 
-app.listen(port, ()=>{
-  // process.send(`ready`);
-  console.log(`express ready`)
-})
+app.listen(port, () => {
+  console.log(`[express] ready`);
+});
